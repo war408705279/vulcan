2e48e25a91f50ed59b8622e7b72c64d5d8562688
war408705279 <408705279@qq.com>
2020-08-27 17:04:30 +0800

    添加 file-loader && svgo-loader 配置

--- remax.config.js
+++ remax.config.js
@@ -1,6 +1,10 @@
 const less = require('@remax/plugin-less')
 const path = require('path')
 
+// function resolve (dir) {
+//   return path.join(__dirname, dir)
+// }
+
 module.exports = {
   one: true,
   output: 'dist/' + process.env.REMAX_PLATFORM,
@@ -11,52 +15,59 @@ module.exports = {
     less()
   ],
   configWebpack({ config }) {
-    config.resolve
-      .extensions
-        .merge(['.ts', '.tsx', '.js', '.jsx', '.json'])
-        .end()
-      .alias
-        .merge({
-          '@': path.resolve(__dirname, 'src'),
-          '@public': path.resolve(__dirname, 'public')
-        })
-        .end()
+    config.resolve.extensions
+      .merge(['.ts', '.tsx', '.js', '.jsx', '.json'])
+      .end()
+    config.resolve.alias
+      .merge({
+        '@': path.resolve(__dirname, 'src'),
+        '@public': path.resolve(__dirname, 'public')
+      })
+      .end()
+    // 删掉默认的 module rules
+    // https://github.com/remaxjs/remax/blob/7c7e065b315d82e765a8e015a13f2ae54b2ff49a/packages/remax-cli/src/build/webpack/config.mini.ts#L159-L169
+    config.module.rules.delete('image')
+    config.module.rules.delete('font')
     config.module
-      .rule('css-less-img')
+      .rule('css-font-img')
       .test(/\.(eot|ttf|woff|woff2|png|jpe?g|gif)$/i)
       .issuer({
         test: /\.(css|less)$/
       })
       .use('file-loader')
-      .loader(require.resolve('file-loader'))
+      .loader('file-loader')
       .options({
+        publicPath: '/images/',
+        outputPath: '/images/',
         name: '[name].[hash].[ext]',
         esModule: false
       })
+    // config.module
+    //   .rule('js-svg')
+    //   .test(/\.svg$/i)
+    //   .issuer({
+    //     test: /\.(js|ts)x?$/
+    //   })
+    //   .use('@svgr/webpack')
+    //   .loader('@svgr/webpack')
+    //   .options({
+    //     svgoConfig: {
+    //       plugins: {
+    //         removeViewBox: false
+    //       }
+    //     }
+    //   })
     config.module
-      .rule('js-ts-svg')
-      .test(/\.svg$/i)
-      .issuer({
-        test: /\.(js|ts)x?$/
-      })
-      .use('svgr-webpack')
-      .loader(require.resolve('@svgr/webpack'))
-      .options({
-        svgoConfig: {
-          plugins: {
-            removeViewBox: false
-          }
-        }
-      })
-    config.module
-      .rule('css-less-svg')
+      .rule('css-svg')
       .test(/\.svg$/i)
       .issuer({
         test: /\.(css|less)$/
       })
       .use('file-loader')
-      .loader(require.resolve('file-loader'))
+      .loader('file-loader')
       .options({
+        publicPath: '/images/',
+        outputPath: '/images/',
         name: '[name].[hash].[ext]',
         esModule: false
       })
@@ -64,14 +75,16 @@ module.exports = {
       .use('svgo-loader')
       .loader('svgo-loader')
     config.module
-      .rule('js-ts-file-svg')
+      .rule('js-file-svg')
       .test(/\.file\.svg$/i)
       .issuer({
         test: /\.(js|ts)x?$/
       })
       .use('file-loader')
-      .loader(require.resolve('file-loader'))
+      .loader('file-loader')
       .options({
+        publicPath: '/images/',
+        outputPath: '/images/',
         name: '[name].[hash].[ext]',
         esModule: false
       })
@@ -79,14 +92,16 @@ module.exports = {
       .use('svgo-loader')
       .loader('svgo-loader')
     config.module
-      .rule('js-ts-img')
+      .rule('js-img')
       .test(/\.(png|jpe?g|gif)$/i)
       .issuer({
         test: /\.(js|ts)x?$/
       })
       .use('file-loader')
-      .loader(require.resolve('file-loader'))
+      .loader('file-loader')
       .options({
+        publicPath: '/images/',
+        outputPath: '/images/',
         name: '[name].[hash].[ext]',
         esModule: false
       })
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,16 +4,29 @@
 
 import React from 'react'
 
-import { View } from 'remax/one'
+import { View, Image } from 'remax/one'
 
 import styles from './index.less'
 
+import testOne from './_image/test.png'
+import testTwo from './_image/test.file.svg'
+// import TestIconSVG from './_image/test.svg'
+
 export default function Index() {
   return (
-    <View
-      className={styles.mainWrapper}
-    >
-      首页 - TODO
-    </View>
+    <>
+      <View
+        className={styles.mainWrapper}
+      >
+        {/* <TestIconSVG /> */}
+        <Image src={testTwo} style={{width: '50px', height: '50px'}} />
+        <Image src={testOne} style={{width: '50px', height: '50px'}} />
+      </View>
+      <View
+        className={styles.mainWrapper}
+      >
+        首页 - TODO
+      </View>
+    </>
   )
 }
