c0399a9e38f75366074e3cf0150a51970af4db3c
war408705279 <408705279@qq.com>
2021-01-26 16:11:04 +0800

    修改 ui/index 页面，添加各个子页面入口

--- src/pages/ui/index.tsx
+++ src/pages/ui/index.tsx
@@ -4,20 +4,144 @@
 
 import React from 'react'
 
+import { View } from 'remax/one'
+
 import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
-import UnderConstruction from '@/components/UnderConstruction'
+
+import Cell from '@/ui/Cell'
 
 import { nameMap } from '@/constants/route'
 
-import './index.less'
+import styles from './index.less'
 
 export default function Ui() {
   return (
     <Scaffold appBar={<AppBar title={nameMap.ui} />}>
-      <UnderConstruction
-        tip="页面施工中..."
-      />
+      <View className={styles.main}>
+        <General />
+        <DataEntry />
+        <DataDisplay />
+        <Feedback />
+      </View>
     </Scaffold>
+  )
+}
+
+function General() {
+  return (
+    <>
+      <View className={styles.title}>
+        General
+      </View>
+      <View className={styles.cells}>
+        <Cell
+          className={styles.cell}
+          label="Cell"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Icon"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Popup"
+          arrow
+        />
+      </View>
+    </>
+  )
+}
+
+function DataEntry() {
+  return (
+    <>
+      <View className={styles.title}>
+        Data Entry
+      </View>
+      <View className={styles.cells}>
+        <Cell
+          className={styles.cell}
+          label="Input"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Textarea"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Radio"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Checkbox"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Switch"
+          arrow
+        />
+      </View>
+    </>
+  )
+}
+
+function DataDisplay() {
+  return (
+    <>
+      <View className={styles.title}>
+        Data Display
+      </View>
+      <View className={styles.cells}>
+        <Cell
+          className={styles.cell}
+          label="Tag"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Tabs"
+          arrow
+        />
+      </View>
+    </>
+  )
+}
+
+function Feedback() {
+  return (
+    <>
+      <View className={styles.title}>
+        Feedback
+      </View>
+      <View className={styles.cells}>
+        <Cell
+          className={styles.cell}
+          label="Loading"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Page Loading"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Result"
+          arrow
+        />
+        <Cell
+          className={styles.cell}
+          label="Toast"
+          arrow
+        />
+      </View>
+    </>
   )
 }
