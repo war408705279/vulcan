763544f9007cdbfde12a87caac6fba7ad1c257fb
war408705279 <408705279@qq.com>
2021-01-29 11:23:01 +0800

    完善 ui/tag 页面细节 && 添加 tag demos

--- src/pages/ui/pages/tag/index.tsx
+++ src/pages/ui/pages/tag/index.tsx
@@ -2,7 +2,7 @@
  * @file ui tag page
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View } from 'remax/one'
 
@@ -10,11 +10,25 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Tag, { CheckableTag } from '@/ui/Tag'
+
 import { nameMap } from '@/constants/route'
+
+import {
+  dangerColor,
+  warningColor,
+  successColor,
+  primaryColor,
+  blueTwo,
+  greenTwo
+} from '@/utils/styles/color'
 
 import styles from './index.less'
 
 export default function UiTag() {
+  const [checkedOne, setCheckedOne] = useState(false)
+  const [checkedTwo, setCheckedTwo] = useState(false)
+
   return (
     <Scaffold
       appBar={
@@ -25,7 +39,83 @@ export default function UiTag() {
       }
     >
       <View className={styles.main}>
-        Tag 组件
+        <View className={styles.title}>
+          Basic
+        </View>
+        <View className={styles.content}>
+          <Tag className={styles.tag}>Vulcan</Tag>
+        </View>
+
+        <View className={styles.title}>
+          Round
+        </View>
+        <View className={styles.content}>
+          <Tag
+            className={styles.tag}
+            round
+          >
+            Vulcan
+          </Tag>
+        </View>
+
+        <View className={styles.title}>
+          Color
+        </View>
+        <View className={styles.content}>
+          <Tag
+            className={styles.tag}
+            color={dangerColor}
+          >
+            Vulcan
+          </Tag>
+          <Tag
+            className={styles.tag}
+            color={warningColor}
+          >
+            Vulcan
+          </Tag>
+          <Tag
+            className={styles.tag}
+            color={successColor}
+          >
+            Vulcan
+          </Tag>
+        </View>
+
+        <View className={styles.title}>
+          Checkable
+        </View>
+        <View className={styles.content}>
+          <CheckableTag
+            className={styles.tag}
+            color={primaryColor}
+            checkedColor={blueTwo}
+            checked={checkedOne}
+            onChange={setCheckedOne}
+          >
+            Vulcan
+          </CheckableTag>
+
+          <CheckableTag
+            className={styles.tag}
+            round
+            color={successColor}
+            checkedColor={greenTwo}
+            checked={checkedTwo}
+            onChange={setCheckedTwo}
+          >
+            Vulcan
+          </CheckableTag>
+
+          <CheckableTag
+            className={styles.tag}
+            color={primaryColor}
+            checkedColor={blueTwo}
+            disabled
+          >
+            Vulcan
+          </CheckableTag>
+        </View>
       </View>
     </Scaffold>
   )
