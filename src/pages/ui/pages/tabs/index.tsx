9de3c9336755feb7c17680825258aa8dc5b97b2c
war408705279 <408705279@qq.com>
2021-01-29 18:15:00 +0800

    完善 ui/tabs 页面 && 添加 tabs demo

--- src/pages/ui/pages/tabs/index.tsx
+++ src/pages/ui/pages/tabs/index.tsx
@@ -10,6 +10,8 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Tabs, { TabPane } from '@/ui/Tabs'
+
 import { nameMap } from '@/constants/route'
 
 import styles from './index.less'
@@ -25,7 +27,109 @@ export default function UiTabs() {
       }
     >
       <View className={styles.main}>
-        Tabs 组件
+        <View className={styles.title}>
+          Basic
+        </View>
+        <View className={styles.container}>
+          <Tabs defaultValue="Vulcan">
+            <TabPane
+              tab="Vulcan"
+              value="Vulcan"
+            >
+              <View className={styles.content}>
+                Vulcan
+              </View>
+            </TabPane>
+            <TabPane
+              tab="Spock"
+              value="Spock"
+            >
+              <View className={styles.content}>
+                Spock
+              </View>
+            </TabPane>
+          </Tabs>
+        </View>
+
+        <View className={styles.title}>
+          Small Size
+        </View>
+        <View className={styles.container}>
+          <Tabs
+            defaultValue="Vulcan"
+            size="small"
+          >
+            <TabPane
+              tab="Vulcan"
+              value="Vulcan"
+            >
+              <View className={styles.content}>
+                Vulcan
+              </View>
+            </TabPane>
+            <TabPane
+              tab="Spock"
+              value="Spock"
+            >
+              <View className={styles.content}>
+                Spock
+              </View>
+            </TabPane>
+          </Tabs>
+        </View>
+
+        <View className={styles.title}>
+          Default Theme
+        </View>
+        <View className={styles.container}>
+          <Tabs
+            defaultValue="Vulcan"
+            theme="default"
+          >
+            <TabPane
+              tab="Vulcan"
+              value="Vulcan"
+            >
+              <View className={styles.content}>
+                Vulcan
+              </View>
+            </TabPane>
+            <TabPane
+              tab="Spock"
+              value="Spock"
+            >
+              <View className={styles.content}>
+                Spock
+              </View>
+            </TabPane>
+          </Tabs>
+        </View>
+
+        <View className={styles.title}>
+          Auto Destroy
+        </View>
+        <View className={styles.container}>
+          <Tabs defaultValue="Vulcan">
+            <TabPane
+              tab="Vulcan"
+              value="Vulcan"
+              autoDestroy
+            >
+              <View className={styles.content}>
+                Vulcan
+              </View>
+            </TabPane>
+            <TabPane
+              tab="Spock"
+              value="Spock"
+              autoDestroy
+            >
+              <View className={styles.content}>
+                Spock
+              </View>
+            </TabPane>
+          </Tabs>
+        </View>
       </View>
     </Scaffold>
   )
