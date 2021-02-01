d9d4b658f2c73348c535871b25ccc64c91e7b1db
war408705279 <408705279@qq.com>
2021-02-01 11:14:30 +0800

    添加 ui/result 页面细节 && 添加 demos

--- src/pages/ui/pages/result/index.tsx
+++ src/pages/ui/pages/result/index.tsx
@@ -10,11 +10,19 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Result from '@/ui/Result'
+
 import { nameMap } from '@/constants/route'
 
 import styles from './index.less'
 
 export default function UiResult() {
+  const extraView = (
+    <View className={styles.extra}>
+      Extra
+    </View>
+  )
+
   return (
     <Scaffold
       appBar={
@@ -25,7 +33,53 @@ export default function UiResult() {
       }
     >
       <View className={styles.main}>
-        Result 组件
+        <View className={styles.title}>
+          Info
+        </View>
+        <View className={styles.content}>
+          <Result
+            icon="info"
+            title="Title"
+            subTitle="SubTitle"
+            extra={extraView}
+          />
+        </View>
+
+        <View className={styles.title}>
+          Success
+        </View>
+        <View className={styles.content}>
+          <Result
+            icon="success"
+            title="Title"
+            subTitle="SubTitle"
+            extra={extraView}
+          />
+        </View>
+
+        <View className={styles.title}>
+          Error
+        </View>
+        <View className={styles.content}>
+          <Result
+            icon="error"
+            title="Title"
+            subTitle="SubTitle"
+            extra={extraView}
+          />
+        </View>
+
+        <View className={styles.title}>
+          Time
+        </View>
+        <View className={styles.content}>
+          <Result
+            icon="time"
+            title="Title"
+            subTitle="SubTitle"
+            extra={extraView}
+          />
+        </View>
       </View>
     </Scaffold>
   )
