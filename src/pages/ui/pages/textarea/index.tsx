d6cd884b629506f1bf1ce7ba3f8a30a54cd8d239
war408705279 <408705279@qq.com>
2021-02-04 11:42:02 +0800

    完善 ui/textarea 页面 && 添加 demos

--- src/pages/ui/pages/textarea/index.tsx
+++ src/pages/ui/pages/textarea/index.tsx
@@ -2,7 +2,7 @@
  * @file ui textarea page
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View } from 'remax/one'
 
@@ -10,11 +10,19 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Textarea from '@/ui/Textarea'
+
 import { nameMap } from '@/constants/route'
+
+import { useToast } from '@/utils/toast'
+import { successColor } from '@/utils/styles/color'
 
 import styles from './index.less'
 
 export default function UiTextarea() {
+  const [value, setValue] = useState('')
+  const showToast = useToast()
+
   return (
     <Scaffold
       appBar={
@@ -25,7 +33,161 @@ export default function UiTextarea() {
       }
     >
       <View className={styles.main}>
-        Textarea 组件
+        <View className={styles.title}>
+          Basic
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            placeholder="Please enter"
+          />
+        </View>
+
+        <View className={styles.title}>
+          With Label
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+          />
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            leftStyle={{
+              lineHeight: 1,
+              alignItems: 'flex-start'
+            }}
+          />
+        </View>
+
+        <View className={styles.title}>
+          Max Length
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            maxLength={140}
+            placeholder="You can only input 140 characters"
+          />
+        </View>
+
+        <View className={styles.title}>
+          Textarea Align
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            textareaAlign="right"
+            placeholder="Please enter"
+          />
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            textareaAlign="center"
+            placeholder="Please enter"
+          />
+        </View>
+
+        <View className={styles.title}>
+          Disabled
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            value="Has been disabled"
+            disabled
+          />
+        </View>
+
+        <View className={styles.title}>
+          With Required
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            required
+          />
+        </View>
+
+        <View className={styles.title}>
+          With Icon
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            icon="email"
+          />
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            icon="email"
+            iconActiveColor={successColor}
+          />
+        </View>
+
+        <View className={styles.title}>
+          With Extra
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            extra="Extra Node"
+          />
+        </View>
+
+        <View className={styles.title}>
+          With Description
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            description="Live long and prosper"
+          />
+        </View>
+
+        <View className={styles.title}>
+          Change & Blur
+        </View>
+        <View className={styles.items}>
+          <Textarea
+            className={styles.item}
+            textareaCls={styles.textarea}
+            label="Label"
+            placeholder="Please enter"
+            value={value}
+            onChange={(e: any) => setValue(e.target.value)}
+            onBlur={() => {
+              showToast({
+                tip: `Content: ${value || 'none'}`
+              })
+            }}
+          />
+        </View>
       </View>
     </Scaffold>
   )
