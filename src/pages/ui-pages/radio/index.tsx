9d31bb90af641c3e31b81e0a569feda9a8512b35
war408705279 <408705279@qq.com>
2021-02-05 11:07:34 +0800

    完善 ui/radio 组件页面 && 添加 demos

--- src/pages/ui-pages/radio/index.tsx
+++ src/pages/ui-pages/radio/index.tsx
@@ -2,7 +2,7 @@
  * @file ui radio page
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View } from 'remax/one'
 
@@ -10,11 +10,29 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Radio, { RadioGroup } from '@/ui/Radio'
+
 import { nameMap } from '@/constants/route'
+
+import { useToast } from '@/utils/toast'
+import { useOnChange } from '@/utils/hooks'
 
 import styles from './index.less'
 
 export default function UiRadio() {
+  const showToast = useToast()
+
+  const [checked, setChecked] = useState(false)
+  const [name, setName] = useState('Kirk')
+
+  useOnChange(() => {
+    if (!name) return
+
+    showToast({
+      tip: `Hi, I am ${name}`
+    })
+  }, [name])
+
   return (
     <Scaffold
       appBar={
@@ -25,7 +43,91 @@ export default function UiRadio() {
       }
     >
       <View className={styles.main}>
-        Radio 组件
+        <View className={styles.title}>
+          Basic
+        </View>
+        <View className={styles.items}>
+          <Radio
+            value="Radio"
+            checked
+          >
+            Radio
+          </Radio>
+        </View>
+
+        <View className={styles.title}>
+          Checkable
+        </View>
+        <View className={styles.items}>
+          <Radio
+            value="Radio"
+            checked={checked}
+            onChange={(v: boolean) => setChecked(v)}
+          >
+            Radio
+          </Radio>
+        </View>
+
+        <View className={styles.title}>
+          Group
+        </View>
+        <View className={styles.items}>
+          <RadioGroup defaultValue="Kirk">
+            <Radio value="Kirk">
+              Kirk
+            </Radio>
+            <Radio value="Spock">
+              Spock
+            </Radio>
+            <Radio value="McCoy">
+              McCoy
+            </Radio>
+          </RadioGroup>
+        </View>
+
+        <View className={styles.title}>
+          Group Button
+        </View>
+        <View className={styles.items}>
+          <RadioGroup
+            type="button"
+            defaultValue="Kirk"
+          >
+            <Radio value="Kirk">
+              Kirk
+            </Radio>
+            <Radio value="Spock">
+              Spock
+            </Radio>
+            <Radio value="McCoy">
+              McCoy
+            </Radio>
+          </RadioGroup>
+        </View>
+
+        <View className={styles.title}>
+          Group Radio Change
+        </View>
+        <View className={styles.items}>
+          <RadioGroup
+            type="button"
+            defaultValue="Kirk"
+            onChange={(v: any) => {
+              if (v === name) return
+              setName(v)
+            }}
+          >
+            <Radio value="Kirk">
+              Kirk
+            </Radio>
+            <Radio value="Spock">
+              Spock
+            </Radio>
+            <Radio value="McCoy">
+              McCoy
+            </Radio>
+          </RadioGroup>
+        </View>
       </View>
     </Scaffold>
   )
