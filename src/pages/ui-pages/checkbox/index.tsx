89c0dc0368249b3e4f1392e0661e75a449500ebe
war408705279 <408705279@qq.com>
2021-02-05 14:46:45 +0800

    完善 ui/checkbox 页面细节 && 添加 demos

--- src/pages/ui-pages/checkbox/index.tsx
+++ src/pages/ui-pages/checkbox/index.tsx
@@ -2,7 +2,7 @@
  * @file ui checkbox page
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import { View } from 'remax/one'
 
@@ -10,11 +10,27 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Checkbox, { CheckboxGroup } from '@/ui/Checkbox'
+
 import { nameMap } from '@/constants/route'
+
+import { useToast } from '@/utils/toast'
+import { useOnChange } from '@/utils/hooks'
 
 import styles from './index.less'
 
 export default function UiCheckbox() {
+  const showToast = useToast()
+
+  const [checked, setChecked] = useState(false)
+  const [names, setNames] = useState(['Kirk'])
+
+  useOnChange(() => {
+    showToast({
+      tip: `${names.join(', ') || 'none'}`
+    })
+  }, [names])
+
   return (
     <Scaffold
       appBar={
@@ -25,7 +41,67 @@ export default function UiCheckbox() {
       }
     >
       <View className={styles.main}>
-        Checkbox 组件
+        <View className={styles.title}>
+          Basic
+        </View>
+        <View className={styles.items}>
+          <Checkbox
+            value="Checkbox"
+            checked
+          >
+            Checkbox
+          </Checkbox>
+        </View>
+
+        <View className={styles.title}>
+          Checkable
+        </View>
+        <View className={styles.items}>
+          <Checkbox
+            value="Checkbox"
+            checked={checked}
+            onChange={(v: boolean) => setChecked(v)}
+          >
+            Checkbox
+          </Checkbox>
+        </View>
+
+        <View className={styles.title}>
+          Group
+        </View>
+        <View className={styles.items}>
+          <CheckboxGroup defaultValues={['Kirk']}>
+            <Checkbox value="Kirk">
+              Kirk
+            </Checkbox>
+            <Checkbox value="Spock">
+              Spock
+            </Checkbox>
+            <Checkbox value="McCoy">
+              McCoy
+            </Checkbox>
+          </CheckboxGroup>
+        </View>
+
+        <View className={styles.title}>
+          Group Change
+        </View>
+        <View className={styles.items}>
+          <CheckboxGroup
+            defaultValues={names}
+            onChange={(v: any) => setNames(v)}
+          >
+            <Checkbox value="Kirk">
+              Kirk
+            </Checkbox>
+            <Checkbox value="Spock">
+              Spock
+            </Checkbox>
+            <Checkbox value="McCoy">
+              McCoy
+            </Checkbox>
+          </CheckboxGroup>
+        </View>
       </View>
     </Scaffold>
   )
