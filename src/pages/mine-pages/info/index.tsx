ffd779abe3472001b574007a2dc148bd489b96cb
war408705279 <408705279@qq.com>
2021-02-07 20:49:15 +0800

    完善 mine-pages/info 页面

--- src/pages/mine-pages/info/index.tsx
+++ src/pages/mine-pages/info/index.tsx
@@ -4,14 +4,19 @@
 
 import React from 'react'
 
+import { View, Image } from 'remax/one'
+
 import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
-import UnderConstruction from '@/components/UnderConstruction'
+
+import Cell from '@/ui/Cell'
 
 import { nameMap } from '@/constants/route'
 
-import './index.less'
+import ImgCodingDog from './images/img-coding-dog.jpg'
+
+import styles from './index.less'
 
 export default function MineInfo() {
   return (
@@ -23,9 +28,80 @@ export default function MineInfo() {
         />
       }
     >
-      <UnderConstruction
-        tip="页面施工中..."
-      />
+      <View className={styles.main}>
+        <View className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src={ImgCodingDog}
+          />
+        </View>
+
+        <View className={styles.infoContainer}>
+          <Cell
+            className={styles.info}
+            label="Name:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              Macco
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Gender:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              Male
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Birthday:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              1992/3/27
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Native place:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              Taiyuan, Shanxi Province
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Residence place:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              Fengxian, Shanghai
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Position:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              FE Development Engineer
+            </View>
+          </Cell>
+          <Cell
+            className={styles.info}
+            label="Github:"
+            leftStyle={{ width: '230rpx' }}
+          >
+            <View className={styles.content}>
+              https://github.com/war408705279
+            </View>
+          </Cell>
+        </View>
+      </View>
     </Scaffold>
   )
 }
