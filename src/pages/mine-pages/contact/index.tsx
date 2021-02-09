d036a7dc822d48829383d32a7c55c6be36817af3
war408705279 <408705279@qq.com>
2021-02-09 08:52:35 +0800

    完善 mine-pages/contact 页面细节，添加文案 && 样式

--- src/pages/mine-pages/contact/index.tsx
+++ src/pages/mine-pages/contact/index.tsx
@@ -4,16 +4,24 @@
 
 import React from 'react'
 
+import { View } from 'remax/one'
+
 import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
-import UnderConstruction from '@/components/UnderConstruction'
+
+import Cell from '@/ui/Cell'
 
 import { nameMap } from '@/constants/route'
 
-import './index.less'
+import styles from './index.less'
 
 export default function MineContact() {
+  const leftStyle = {
+    width: '140rpx',
+    flex: '0 0 auto'
+  }
+
   return (
     <Scaffold
       appBar={
@@ -23,9 +31,47 @@ export default function MineContact() {
         />
       }
     >
-      <UnderConstruction
-        tip="页面施工中..."
-      />
+      <View className={styles.main}>
+        <Cell
+          className={styles.item}
+          label="Mobile:"
+          leftStyle={leftStyle}
+        >
+          <View className={styles.content}>
+            13585954943
+          </View>
+        </Cell>
+
+        <Cell
+          className={styles.item}
+          label="Mail:"
+          leftStyle={leftStyle}
+        >
+          <View className={styles.content}>
+            jiayizhen@qiniu.com
+          </View>
+        </Cell>
+
+        <Cell
+          className={styles.item}
+          label="Wechat:"
+          leftStyle={leftStyle}
+        >
+          <View className={styles.content}>
+            Jia-_-YiZhen
+          </View>
+        </Cell>
+
+        <Cell
+          className={styles.item}
+          label="Address:"
+          leftStyle={leftStyle}
+        >
+          <View className={styles.content}>
+            上海市奉贤区一个贼偏的地方
+          </View>
+        </Cell>
+      </View>
     </Scaffold>
   )
 }
