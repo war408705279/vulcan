005dfda713b8fc30fb9fc056217a8a904782455c
war408705279 <408705279@qq.com>
2021-02-11 12:43:24 +0800

    完善 mine-pages/assessment 页面 && 添加文案

--- src/pages/mine-pages/assessment/index.tsx
+++ src/pages/mine-pages/assessment/index.tsx
@@ -10,6 +10,8 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import BackLeading from '@/components/AppBar/BackLeading'
 
+import Cell from '@/ui/Cell'
+
 import { nameMap } from '@/constants/route'
 
 import styles from './index.less'
@@ -25,7 +27,35 @@ export default function MineAssessment() {
       }
     >
       <View className={styles.main}>
-        自我评价
+        <Cell className={styles.item}>
+          <View className={styles.content}>
+            我是一名前端 爱吃小熊饼干 (●ﾟωﾟ●)
+          </View>
+        </Cell>
+
+        <Cell className={styles.item}>
+          <View className={styles.content}>
+            上班只为下班 工资刚够早餐 (๑•̀ㅂ•́)و✧
+          </View>
+        </Cell>
+
+        <Cell className={styles.item}>
+          <View className={styles.content}>
+            每晚都要加班 只求线上平安 ヾ(=･ω･=)o
+          </View>
+        </Cell>
+
+        <Cell className={styles.item}>
+          <View className={styles.content}>
+            头发快要掉完 前端真的好难 o(TωT)o
+          </View>
+        </Cell>
+
+        <Cell className={styles.item}>
+          <View className={styles.content}>
+            只是无名前端 写代码的憨憨 ─=≡Σ(((つ•̀ω•́)つ))
+          </View>
+        </Cell>
       </View>
     </Scaffold>
   )
