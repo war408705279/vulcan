8d8bccf2380a81e532a4a7ea4048a8d78512a99e
war408705279 <408705279@qq.com>
2021-02-16 09:22:25 +0800

    重构 mine 首页样式 && dom 渲染

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -10,13 +10,11 @@ import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
 import Navigator from '@/components/Navigator'
 
+import Cell from '@/ui/Cell'
+
 import { nameMap, routeMap } from '@/constants/route'
 
-import InfoIcon from './images/icon-info.svg'
-import SkillIcon from './images/icon-skill.svg'
-import ContactIcon from './images/icon-contact.svg'
-import HobbyIcon from './images/icon-hobby.svg'
-import AssessmentIcon from './images/icon-assessment.svg'
+import IconVulcan from './images/icon-vulcan.jpg'
 
 import styles from './index.less'
 
@@ -24,85 +22,70 @@ export default function Mine() {
   return (
     <Scaffold appBar={<AppBar title={nameMap.mine} />}>
       <View className={styles.main}>
-        <Navigator
-          className={styles.navigator}
-          url={routeMap.mine_info}
-          action="navigate"
-        >
-          <Card
-            icon={InfoIcon}
-            tip="基本信息"
+        <View className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            src={IconVulcan}
           />
-        </Navigator>
+        </View>
 
-        <Navigator
-          className={styles.navigator}
-          url={routeMap.mine_skill}
-          action="navigate"
-        >
-          <Card
-            icon={SkillIcon}
-            tip="技能"
-          />
-        </Navigator>
+        <View className={styles.cells}>
+          <Navigator
+            className={styles.cell}
+            url={routeMap.mine_info}
+            action="navigate"
+          >
+            <Cell
+              label="Basic Info"
+              arrow
+            />
+          </Navigator>
 
-        <Navigator
-          className={styles.navigator}
-          url={routeMap.mine_contact}
-          action="navigate"
-        >
-          <Card
-            icon={ContactIcon}
-            tip="联系方式"
-          />
-        </Navigator>
+          <Navigator
+            className={styles.cell}
+            url={routeMap.mine_skill}
+            action="navigate"
+          >
+            <Cell
+              label="Skills"
+              arrow
+            />
+          </Navigator>
 
-        <Navigator
-          className={styles.navigator}
-          url={routeMap.mine_hobby}
-          action="navigate"
-        >
-          <Card
-            icon={HobbyIcon}
-            tip="爱好"
-          />
-        </Navigator>
+          <Navigator
+            className={styles.cell}
+            url={routeMap.mine_contact}
+            action="navigate"
+          >
+            <Cell
+              label="Contact"
+              arrow
+            />
+          </Navigator>
 
-        <Navigator
-          className={styles.navigator}
-          url={routeMap.mine_assessment}
-          action="navigate"
-        >
-          <Card
-            icon={AssessmentIcon}
-            tip="自我评价"
-          />
-        </Navigator>
-      </View>
-    </Scaffold>
-  )
-}
+          <Navigator
+            className={styles.cell}
+            url={routeMap.mine_hobby}
+            action="navigate"
+          >
+            <Cell
+              label="Hobby"
+              arrow
+            />
+          </Navigator>
 
-type CardProps = {
-  icon: string
-  tip: string
-}
-
-function Card({
-  icon,
-  tip
-}: CardProps) {
-  return (
-    <View className={styles.card}>
-      <View className={styles.info}>
-        <Image
-          className={styles.icon}
-          src={icon}
-        />
-        <View className={styles.tip}>
-          {tip}
+          <Navigator
+            className={styles.cell}
+            url={routeMap.mine_assessment}
+            action="navigate"
+          >
+            <Cell
+              label="Assessment"
+              arrow
+            />
+          </Navigator>
         </View>
       </View>
-    </View>
+    </Scaffold>
   )
 }
