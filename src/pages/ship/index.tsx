796cdc9b2421dddd18091582377172012042fc04
war408705279 <408705279@qq.com>
2021-02-26 09:20:04 +0800

    添加 ship/index 部分细节内容

--- src/pages/ship/index.tsx
+++ src/pages/ship/index.tsx
@@ -4,18 +4,47 @@
 
 import React from 'react'
 
+import { View } from 'remax/one'
+
 import Scaffold from '@/components/Scaffold'
 import AppBar from '@/components/AppBar'
-import UnderConstruction from '@/components/UnderConstruction'
+
+import Cell from '@/ui/Cell'
 
 import { nameMap } from '@/constants/route'
 
-import './index.less'
+import styles from './index.less'
 
 export default function Ship() {
   return (
     <Scaffold appBar={<AppBar title={nameMap.ship} />}>
-      <UnderConstruction tip="页面开发中..." />
+      <View className={styles.main}>
+        <View className={styles.title}>
+          22 世纪
+        </View>
+        <View className={styles.cells}>
+          <Cell
+            className={styles.cell}
+            label="恒河级 - Ganges Class"
+            arrow
+          />
+          <Cell
+            className={styles.cell}
+            label="无畏型 - Intrepid Type"
+            arrow
+          />
+          <Cell
+            className={styles.cell}
+            label="NX 级 - NX Class"
+            arrow
+          />
+          <Cell
+            className={styles.cell}
+            label="代达罗斯级 - Daedalus Class"
+            arrow
+          />
+        </View>
+      </View>
     </Scaffold>
   )
 }
