b9954996203aa20dc790b544419d3c938f28cef9
war408705279 <408705279@qq.com>
2021-02-28 10:12:45 +0800

    添加 pages/ship/page-data.ts file

---
+++ src/pages/ship/page-data.ts
@@ -0,0 +1,189 @@
+/**
+ * @file page-data 相关
+ */
+
+export type DataItemType = {
+  code?: string
+  label: string
+}
+
+export type DataType = {
+  title: string,
+  list: DataItemType[]
+}
+
+export const data: DataType[] = [{
+  title: '22 世纪',
+  list: [{
+    code: '',
+    label: '恒河级 - Ganges Class'
+  }, {
+    code: '',
+    label: '无畏型 - Intrepid Type'
+  }, {
+    code: '',
+    label: 'NX 级 - NX Class'
+  }, {
+    code: '',
+    label: '代达罗斯级 - Daedalus Class'
+  }]
+}, {
+  title: '23 世纪',
+  list: [{
+    code: '',
+    label: '宪法级 - Constitution Class'
+  }, {
+    code: '',
+    label: '宪法级改 - Constitution Class Retrofit'
+  }, {
+    code: '',
+    label: '精进级 - Excelsior Class'
+  }, {
+    code: '',
+    label: '精进级改 - Excelsior Class Retrofit'
+  }, {
+    code: '',
+    label: '星座级 - Constellation Class'
+  }, {
+    code: '',
+    label: '联邦级 - Federation Class'
+  }, {
+    code: '',
+    label: '萨拉丁 / 赫尔墨斯级 - Saladin / Hermes Class'
+  }, {
+    code: '',
+    label: '米兰达级 - Miranda Class'
+  }, {
+    code: '',
+    label: '奥伯特级 - Oberth Class'
+  }, {
+    code: '',
+    label: '安塔尔型 - Antares Type'
+  }, {
+    code: '',
+    label: '悉尼级 - Sydney Class'
+  }, {
+    code: '',
+    label: '克罗斯菲尔德级 - Crossfield Class'
+  }, {
+    code: '',
+    label: '沃克级 - Walker Class'
+  }, {
+    code: '',
+    label: '谢帕德级 - Shepard Class'
+  }, {
+    code: '',
+    label: '马拉霍夫斯基级 - Malachowski Class'
+  }, {
+    code: '',
+    label: '麦基级 - Magee Class'
+  }, {
+    code: '',
+    label: '卡德纳斯级 - Cardenas Class'
+  }, {
+    code: '',
+    label: '尼米兹级 - Nimitz Class'
+  }, {
+    code: '',
+    label: '胡佛级 - Hoover Class'
+  }, {
+    code: '',
+    label: '恩格尔级 - Engle Class'
+  }]
+}, {
+  title: '24 世纪',
+  list: [{
+    code: '',
+    label: '大使级 - Ambassador Class'
+  }, {
+    code: '',
+    label: '半人马级 - Centaur Class'
+  }, {
+    code: '',
+    label: '卡瑞型 - Curry Type'
+  }, {
+    code: '',
+    label: '尼亚加拉级 - Niagara Class'
+  }, {
+    code: '',
+    label: '自由级 - Freedom Class'
+  }, {
+    code: '',
+    label: '夏延级 - Cheyenne Class'
+  }, {
+    code: '',
+    label: '新奥尔良级 - New Orleans Class'
+  }, {
+    code: '',
+    label: '渡鸦型 - Raven Type'
+  }, {
+    code: '',
+    label: '春田级 - Springfield Class'
+  }, {
+    code: '',
+    label: '银河级 - Galaxy Class'
+  }, {
+    code: '',
+    label: '星云级 - Nebula Class'
+  }, {
+    code: '',
+    label: '多瑙河级 - Danube Class'
+  }, {
+    code: '',
+    label: '光明级 - Akira Class'
+  }, {
+    code: '',
+    label: '新星级 - Nova Class'
+  }, {
+    code: '',
+    label: '无畏级 - Intrepid Class'
+  }, {
+    code: '',
+    label: '挪威级 - Norway Class'
+  }, {
+    code: '',
+    label: '军刀级 - Saber Class'
+  }, {
+    code: '',
+    label: '气动级 - Steamrunner Class'
+  }, {
+    code: '',
+    label: '奥林匹克级 - Olympic Class'
+  }, {
+    code: '',
+    label: '挑战级 - Defiant Class'
+  }, {
+    code: '',
+    label: '元首级 - Sovereign Class'
+  }, {
+    code: '',
+    label: '普罗米修斯级 - Prometheus Class'
+  }, {
+    code: '',
+    label: '加利福尼亚级 - California Class'
+  }, {
+    code: '',
+    label: '国会级 - Parliament Class'
+  }, {
+    code: '',
+    label: '好奇级 - Curiosity Class'
+  }, {
+    code: '',
+    label: '瓦伦贝里级 - Wallenberg Class'
+  }, {
+    code: '',
+    label: '调查级 - Inquiry Class'
+  }]
+}, {
+  title: '26 世纪',
+  list: [{
+    code: '',
+    label: '宇宙级 - Universe Class'
+  }]
+}, {
+  title: '29 世纪',
+  list: [{
+    code: '',
+    label: '威尔斯级 - Wells Class'
+  }]
+}]
