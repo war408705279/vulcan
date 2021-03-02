93a7e6d6c66a5fd4905329301f6fdad8bfac0885
war408705279 <408705279@qq.com>
2021-03-02 09:06:32 +0800

    添加 ship-data 文案项

--- src/constants/ship-data.ts
+++ src/constants/ship-data.ts
@@ -18,12 +18,29 @@ export const data: DataType = {
     height: '18 米',
     quality: '40000 吨',
     crewNum: '35 人',
-    cruisingSpeed: '曲速 1.5',
-    maxSpeed: '曲速 1.5',
+    cruisingSpeed: '曲速 1.5 级',
+    maxSpeed: '曲速 1.5 级',
     arms: '相位炮',
     defend: '舰壳极化',
     information: '恒河级星舰于 22 世纪被星际舰队广泛使用，其主舰体为少见的三角形，在舰尾两侧连接了两根曲速舱，从船体上方向上倾斜',
     known: null,
     note: '该型号的星舰并没有官方名称，一般用 Warp Delta 代替，但是在大量的媒体资料中该型星舰被命名为恒河级'
+  },
+  IntrepidType: {
+    name: '无畏型',
+    spec: '未知',
+    time: '22 世纪',
+    length: '',
+    width: '',
+    height: '',
+    quality: '',
+    crewNum: '',
+    cruisingSpeed: '曲速 2 级',
+    maxSpeed: '曲速 2 级',
+    arms: '相位加农炮 / 2 具鱼雷发射管',
+    defend: '舰壳极化',
+    information: '无畏型星舰是星际舰队于 22 世纪中叶服役的一批星舰，其主舰体为半圆形，舰尾连接了两根向上翘起的曲速舱，曲速舱的两侧还有两片向下的舰体结构',
+    known: ['未知舷号 - Intrepid - 无畏号'],
+    note: ''
   }
 }
