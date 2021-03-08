e8baacc9824c18e62ba6599c6c90b578caa59603
war408705279 <408705279@qq.com>
2021-03-08 09:02:52 +0800

    添加 ship-data 文案项

--- src/constants/ship-data/29th-century.ts
+++ src/constants/ship-data/29th-century.ts
@@ -5,4 +5,24 @@
 
 import { DataType } from '.'
 
-export const data: DataType = {}
+export const data: DataType = {
+  WellsClass: {
+    name: '威尔斯级',
+    spec: '时空舰',
+    time: '29 世纪',
+    length: '',
+    width: '',
+    height: '',
+    quality: [],
+    crewNum: [],
+    cruisingSpeed: [],
+    maxSpeed: [],
+    arms: [],
+    defend: '',
+    information: '威尔斯级是星际舰队于 29 世纪所服役的一级时空舰，专门用来应付时空入侵现象',
+    known: [
+      'NCV-474439-G / USS Relativity / 联邦时空舰相对号'
+    ],
+    note: '威尔斯级配备了时空传送机，能够将目标在任意的时间和空间内传送'
+  }
+}
