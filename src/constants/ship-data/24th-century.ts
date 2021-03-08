bfc6b7ba5156622c08463875d49542fc70a4c235
war408705279 <408705279@qq.com>
2021-03-08 09:19:40 +0800

    添加 ship-data 文案项

--- src/constants/ship-data/24th-century.ts
+++ src/constants/ship-data/24th-century.ts
@@ -5,4 +5,35 @@
 
 import { DataType } from '.'
 
-export const data: DataType = {}
+export const data: DataType = {
+  AmbassadorClass: {
+    name: '大使级',
+    spec: '重型巡洋舰',
+    time: '24 世纪',
+    length: '',
+    width: '',
+    height: '',
+    quality: [],
+    crewNum: [],
+    cruisingSpeed: [],
+    maxSpeed: [],
+    arms: [
+      '10 条相位阵列 / 2 具光子鱼雷发射管'
+    ],
+    defend: '偏导护盾',
+    information: '大使级星舰是星际舰队在 24 世纪早期到 24 世纪晚期服役的一种重型巡洋舰，在 24 世纪早期的联邦 - 克林贡战争和 2367 年的 Wolf 359 战役中都可以看见大使级的身影',
+    known: [
+      'NX/NCC-10521 / USS Ambassador / 联邦星舰大使号',
+      'NCC-10532 / USS Horatio / 联邦星舰霍雷肖号',
+      'NCC-1701-C / USS Enterprise / 联邦星舰企业号',
+      'NCC-26136 / USS Zhukov / 联邦星舰朱可夫号',
+      'NCC-26198 / USS Valdemar / 联邦星舰瓦尔德马号',
+      'NCC-26510 / USS Yamaguchi / 联邦星舰山口号',
+      'NCC-26517 / USS Excalibur / 联邦星舰圣剑号',
+      'NCC-26531 / USS Exeter / 联邦星舰埃克塞特号',
+      'NCC-26632 / USS Gandhi / 联邦星舰甘地号',
+      'NCC-26849 / USS Adelphi / 联邦星舰阿德菲号'
+    ],
+    note: '大使级的设计和绝大部分的联邦星舰相似，大使级依旧采取碟部 - 工程部 - 曲速舱的设计，其尺寸大小介于精进级和银河级之间。许多大使级的外形在后来的升级中发生了一些小改动，碟部和工程部的连接处稍稍向后移动了一点，两侧的曲速架也以完全相同的方式进行了微微的移动，并在巴萨德采集器周围添加了曲速线圈使其看上去更接近银河级，同时脉冲引擎的颜色也由之前的蓝色变为红色'
+  }
+}
