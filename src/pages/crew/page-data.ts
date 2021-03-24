441118d21f2335c288d97fc2a5a578a6da690090
war408705279 <408705279@qq.com>
2021-03-24 10:30:04 +0800

    添加 crew/page-data 文件

---
+++ src/pages/crew/page-data.ts
@@ -0,0 +1,31 @@
+/**
+ * @file page-data 相关
+ */
+
+export type DataType = {
+  code?: string
+  label: string
+}
+
+export const data: DataType[] = [{
+  code: '',
+  label: '威廉 · 夏特纳 饰 詹姆斯 · T · 柯克'
+}, {
+  code: '',
+  label: '伦纳德 · 尼莫伊 饰 史波克'
+}, {
+  code: '',
+  label: '德福雷斯特 · 凯利 饰 莱昂纳德 · 麦考伊'
+}, {
+  code: '',
+  label: '詹姆斯 · 杜汉 饰 蒙哥马利 · 史考特'
+}, {
+  code: '',
+  label: '乔治 · 竹井 饰 光 · 苏鲁'
+}, {
+  code: '',
+  label: '沃特尔 · 科尼格 饰 帕维尔 · 安德烈维奇 · 契科夫'
+}, {
+  code: '',
+  label: '妮切尔 · 尼科尔斯 饰 尼欧塔 · 乌胡拉'
+}]
