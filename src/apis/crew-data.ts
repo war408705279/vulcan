72fc88a68f3eddc385708d86e0161e8b1d490a60
war408705279 <408705279@qq.com>
2021-03-30 11:47:07 +0800

    添加 crew-data 相关 api 定义

---
+++ src/apis/crew-data.ts
@@ -0,0 +1,57 @@
+/**
+ * @file crew-data api
+ */
+
+import { data as crewData } from '@/constants/crew-data'
+
+export type GetCrewDataOptions = {
+  code: string
+}
+
+export type GetCrewDataResp = {
+  icon?: string
+  // 中文名
+  zhName: string
+  // 英文名
+  enName: string[]
+  // 别称
+  alias: string[]
+  // 国籍
+  nationality: string
+  // 民族
+  national: string
+  // 出生地
+  birthplace: string
+  // 出生日期
+  birthday: string
+  // 星座
+  constellation: string
+  // 身高
+  height: string
+  // 毕业院校
+  graduateSchool: string
+  // 职业
+  profession: string[]
+  // 代表作品
+  representativeWorks: string[]
+  // 主要成就
+  mainAchievements: string[]
+  // 爱好
+  hobby: string[]
+  // 信息
+  information: string[]
+  // 评价
+  evaluation: string[]
+}
+
+export function getCrewData(options: GetCrewDataOptions): Promise<GetCrewDataResp> {
+  const { code } = options
+
+  return new Promise(resolve => {
+    const timeoutID = setTimeout(() => {
+      clearTimeout(timeoutID)
+      const data = crewData[code]
+      resolve(data)
+    }, 1500)
+  })
+}
