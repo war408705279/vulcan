ef11ef3366895df11a7613aa04f70ccc77d6d014
war408705279 <408705279@qq.com>
2021-03-31 11:47:32 +0800

    添加 Leonard Nimoy 信息 && 引入 icon

--- src/constants/crew-data/original.ts
+++ src/constants/crew-data/original.ts
@@ -4,6 +4,7 @@
  */
 
 import IconWilliamShatner from './images/original/icon-william-shatner.jpg'
+import IconLeonardNimoy from './images/original/icon-leonard-nimoy.jpg'
 
 import { DataType } from '.'
 
@@ -58,5 +59,43 @@ export const data: DataType = {
       '他喷饭的幽默演技令人印象深刻',
       '威廉 · 夏特纳从未受过古典音乐训练却能够成为世界音乐的文化符号，这是他最重要的品质'
     ]
+  },
+  LeonardNimoy: {
+    icon: IconLeonardNimoy,
+    zhName: '伦纳德 · 尼莫伊',
+    enName: [
+      'Leonard Nimoy'
+    ],
+    alias: [],
+    nationality: '美国',
+    national: '犹太',
+    birthplace: '波士顿',
+    birthday: '1931 年 3 月 26 日',
+    constellation: '白羊座',
+    height: '',
+    graduateSchool: '波士顿学院（BOSTON COLLEGE）',
+    profession: [
+      '电影导演',
+      '演员',
+      '诗人',
+      '音乐家',
+      '摄影师'
+    ],
+    representativeWorks: [
+      '《星际旅行：初代》'
+    ],
+    mainAchievements: [
+      '一生共获得 9 项大奖，包括',
+      '美国科幻、奇幻及恐怖电影学院（Academy of Science Fiction, Fantasy & Horror Films, USA）的 2010 年土星奖（Saturn Award）、2009 年终身成就奖、1987 年生涯奖等'
+    ],
+    hobby: [],
+    information: [
+      '伦纳德 · 尼莫伊（Leonard Nimoy，1931 年 3 月 26 日 － 2015 年 2 月 27 日），出生于美国波士顿，电影导演、诗人、音乐家和摄影师。他以从 1966 年至 1969 年在美国电视连续剧《星际旅行》中扮演斯波克而成名，在后来的一些电影中他也扮演该角色',
+      '2015 年 2 月 27 日上午，因患慢性阻塞性肺疾病，伦纳德 · 尼莫伊在洛杉矶的家中去世，享年 83 岁',
+      '2015 年 2 月 23 日，尼莫伊在推特上写下了最后的文字："人生就像一座花园。完美时刻曾经有过，却无法长存，除非留在记忆之中。LLAP" LLAP 是 Live Long and Prosper 的缩写，这是尼莫伊发推特的习惯',
+      '伦纳德 · 尼莫伊出生于波士顿，他的父母是从俄罗斯移民到美国的母语为意第绪语的犹太人。他的母亲是一名家庭主妇，他的父亲开了一家理发馆。他从 8 岁左右开始表演，17 岁时获得了一个主角。他在洛杉矶加利福尼亚大学学摄影，但是没有结束他的学业。1953 年他在波士顿学院（BOSTON COLLEGE）获得了教育艺术硕士的学位，又在俄亥俄州的安提俄克大学获得了一个荣誉博士学位。他早年在许多 B 级片和电视剧里担任小角色。他回忆说他参加打牌比赛和医学试验来交付跳舞班的学费。从他的口音中可以听得出他是在波士顿长大的',
+      '他在美国陆军预备队中服役，1955 年 11 月最后以中士的军衔退役。据美国国家档案管理局的报道尼莫伊在美国陆军的服役记录在 1973 年国家人事档案中心的大火中被毁'
+    ],
+    evaluation: []
   }
 }
