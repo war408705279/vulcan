d48a6f5c46467be965878d4c3308a1e306ee868f
war408705279 <408705279@qq.com>
2021-04-09 09:02:15 +0800

    添加 crew-data constants 文件 && 引入

--- src/constants/crew-data/index.ts
+++ src/constants/crew-data/index.ts
@@ -5,11 +5,13 @@
 import { GetCrewDataResp } from '@/apis/crew-data'
 
 import { data as originalData } from './original'
+import { data as kelvinData } from './kelvin'
 
 export type DataType = {
   [key: string]: GetCrewDataResp
 }
 
 export const data: DataType = {
-  ...originalData
+  ...originalData,
+  ...kelvinData
 }
---
+++ src/constants/crew-data/kelvin.ts
@@ -0,0 +1,8 @@
+/**
+ * @file crew-data 相关 kelvin 文件
+ * @description 开尔文时间线系列演员信息
+ */
+
+import { DataType } from '.'
+
+export const data: DataType = {}
